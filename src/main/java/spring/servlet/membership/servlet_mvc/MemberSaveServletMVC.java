8347b264dfe173a52d40c68a9f4e61bbc3d36c8a
JJH <jakebbak@gmail.com>
2023-04-11 20:45:39 +0900

    04/11: Servlet MVC

---
+++ src/main/java/spring/servlet/membership/servlet_mvc/MemberSaveServletMVC.java
@@ -0,0 +1,36 @@
+package spring.servlet.membership.servlet_mvc;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import spring.servlet.membership.domain.member.Member;
+import spring.servlet.membership.domain.repository.MemberRepository;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "memberSaveServletJspMVC", urlPatterns = "/membership/servlet-mvc/members/save")
+public class MemberSaveServletMVC extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        //Model에 데이터 보관
+        req.setAttribute("member", member);
+
+        //viewPath로 이동 및 정보 전달(페이지 이동)
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher(viewPath);
+        requestDispatcher.forward(req, resp);
+    }
+}
