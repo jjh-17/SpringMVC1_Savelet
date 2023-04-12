0c3b7dc9e383da6bcbfb7c04feb062a72c25152f
JJH <jakebbak@gmail.com>
2023-04-12 18:00:01 +0900

    04/12: Front Controller V4 : ModelView를 사용하지 않고 간소화

---
+++ src/main/java/spring/servlet/membership/front_controller/v4/ControllerV4.java
@@ -0,0 +1,12 @@
+package spring.servlet.membership.front_controller.v4;
+
+import spring.servlet.membership.front_controller.ModelView;
+
+import java.util.Map;
+
+//컨트롤러 인터페이스
+public interface ControllerV4 {
+    //V3와 달리 추가 파라미터 존재, String(ViewName) 반환
+    //model을 파라미터로 가짐
+    String process(Map<String, String> map, Map<String, Object> model);
+}
