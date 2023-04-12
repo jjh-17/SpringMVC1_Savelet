d5466acf8bd0e9818f79a616dcef59af87e8c087
JJH <jakebbak@gmail.com>
2023-04-12 15:12:44 +0900

    04/12: Front Controller V2 ==> View 객체 도입

---
+++ src/main/java/spring/servlet/membership/front_controller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,28 @@
+package spring.servlet.membership.front_controller.v2.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import spring.servlet.membership.domain.member.Member;
+import spring.servlet.membership.domain.repository.MemberRepository;
+import spring.servlet.membership.front_controller.MyView;
+import spring.servlet.membership.front_controller.v2.ControllerV2;
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+
+        //Model에 데이터 보관
+        request.setAttribute("members", members);
+
+        //V1과 달리 MyView 객체를 이용하여 페이지 이동 중복 코드 제거
+        return new MyView("/WEB-INF/members.jsp");
+
+    }
+}
---
+++ src/main/java/spring/servlet/membership/front_controller/v2/controller/MemberSaveControllerV2.java
@@ -0,0 +1,30 @@
+package spring.servlet.membership.front_controller.v2.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import spring.servlet.membership.domain.member.Member;
+import spring.servlet.membership.domain.repository.MemberRepository;
+import spring.servlet.membership.front_controller.MyView;
+import spring.servlet.membership.front_controller.v2.ControllerV2;
+import java.io.IOException;
+
+public class MemberSaveControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        //Model에 데이터 보관
+        request.setAttribute("member", member);
+
+        //V1과 달리 MyView 객체를 이용하여 페이지 이동 중복 코드 제거
+        return new MyView("/WEB-INF/views/save-result.jsp");
+    }
+}
