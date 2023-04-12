cf394793f4e9131b395436007d4e33bc7a68adde
JJH <jakebbak@gmail.com>
2023-04-12 15:13:24 +0900

    04/12: Front Controller V2 이상을 위한 View 객체

---
+++ src/main/java/spring/servlet/membership/front_controller/MyView.java
@@ -0,0 +1,23 @@
+package spring.servlet.membership.front_controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+//V2 이상부터 사용하는 뷰 객체 : 컨트롤러에서 뷰로 이동하는 코드 전담
+public class MyView {
+
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewPath);
+        requestDispatcher.forward(request, response);
+    }
+}
